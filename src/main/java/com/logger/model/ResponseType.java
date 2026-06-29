9172972017ccee0d295d464f174afed8f9a44d4f
REXRITZ <rmbhadane33@gmail.com>
2026-06-29 15:06:02 +0530

    modified ResponseType enum for parsing

--- src/main/java/com/logger/model/ResponseType.java
+++ src/main/java/com/logger/model/ResponseType.java
@@ -1,9 +1,33 @@
 package com.logger.model;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public enum ResponseType {
-    SUCCESS,
-    BAD_REQUEST,
-    UNAUTHORIZED,
-    NOT_FOUND,
-    INTERNAL_SERVER_ERROR
+    SUCCESS("200"),
+    CREATED("201"),
+    BAD_REQUEST("400"),
+    UNAUTHORIZED("401"),
+    NOT_FOUND("404"),
+    INTERNAL_SERVER_ERROR("500");
+
+    private final String code;
+
+    private static final Map<String, ResponseType> codeMap = 
+            Arrays.stream(values())
+                  .collect(Collectors.toMap(ResponseType::getCode, Function.identity()));
+
+    ResponseType(String code) {
+        this.code = code;
+    }
+
+    private String getCode() {
+        return code;
+    }
+
+    public static ResponseType parse(String code) {
+        return codeMap.get(code);
+    }
 }
